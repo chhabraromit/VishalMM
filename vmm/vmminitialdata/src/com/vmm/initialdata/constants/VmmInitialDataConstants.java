e8bba24da954923829c65e8ae83453b4165a1f2d
chhabraromit <chhabra.romit@gmail.com>
2016-08-10 09:01:26 +0530

    Adding vmm

---
+++ vmm/vmminitialdata/src/com/vmm/initialdata/constants/VmmInitialDataConstants.java
@@ -0,0 +1,25 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.vmm.initialdata.constants;
+
+/**
+ * Global class for all VmmInitialData constants.
+ */
+public final class VmmInitialDataConstants extends GeneratedVmmInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "vmminitialdata";
+
+	private VmmInitialDataConstants()
+	{
+		//empty
+	}
+}
